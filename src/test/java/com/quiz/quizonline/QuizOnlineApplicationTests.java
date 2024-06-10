384302d2094c43a9015d23e7167d6dbe799a696c
KEVIN SAVALIYA <kscode.2003@gmail.com>
2024-06-10 13:03:32 +0530

    commit

---
+++ src/test/java/com/quiz/quizonline/QuizOnlineApplicationTests.java
@@ -0,0 +1,15 @@
+
+
+package com.quiz.quizonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
